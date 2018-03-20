2d4e141a6a5f91e68f4ee73385e26052455f96b6
sro <sro>
2018-03-20 12:38:05 +0100

    docker image

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #build the docker image
-docker build -t rowshan/eds_e2e rowshan/eds_e2e
+docker build -t rowshan/eds_e2e .
 #Run the test_app image container
-docker run -d -p 5000:5000 rowshan/eds_e2e
+docker run -p 5000:5000 rowshan/eds_e2e
